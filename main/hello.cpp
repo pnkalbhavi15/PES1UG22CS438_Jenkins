b72f53583617d4ecfb95720fa5a15e5c760e4a38
Praneetha Kalbhavi <praneethakalbhavi@gmail.com>
2025-03-18 16:47:45 +0530

    Updated hello.cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout<<"PES1UG22CS438";
   cout << "Hello, World\n";
+  cout<<"PES1UG22CS438";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
